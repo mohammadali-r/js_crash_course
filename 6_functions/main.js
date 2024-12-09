dce4f2b4788d29b25501fa9f8581bf7481b71e74
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-12-10 01:58:32 +0330

    feat: add new some functions (#3)
    Co-authored-by: mohammadalirohanian <rohanian021@gmail.com>

--- 6_functions/main.js
+++ 6_functions/main.js
@@ -2,4 +2,17 @@ function say_hello() {
     console.log("Hello World !")
 }
 
-say_hello()
+// say_hello()
+
+function hello_person(name){
+    alert("Hi " + name)
+}
+
+// hello_person("Alex")
+
+function add(a, b) {
+    return(a + b)
+}
+
+res = add(12, 22)
+console.log(res)
