26b8303eb6e96c7d2bba109bcd6177aaf4de599a
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-12-10 19:04:04 +0330

    feat: add new function (#4)

--- 6_functions/main.js
+++ 6_functions/main.js
@@ -1,18 +1,29 @@
 function say_hello() {
-    console.log("Hello World !")
+  console.log("Hello World !");
 }
 
 // say_hello()
 
-function hello_person(name){
-    alert("Hi " + name)
+function hello_person(name) {
+  alert("Hi " + name);
 }
 
 // hello_person("Alex")
 
 function add(a, b) {
-    return(a + b)
+  return a + b;
 }
 
-res = add(12, 22)
-console.log(res)
+res = add(12, 22);
+console.log(res);
+
+function hello_input() {
+  var name = prompt("Please enter your name: ");
+  if (name == "") {
+    alert("Hi there");
+  } else {
+    alert("Hello " + name);
+  }
+}
+
+hello_input();
