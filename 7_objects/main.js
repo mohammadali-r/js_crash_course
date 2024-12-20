933906e7ba34c58159575aeb7850b705fcabd8b2
mohammadalirohanian <rohanian021@gmail.com>
2024-12-20 23:34:24 +0330

    feat: add some objects stuff to 7_objects directory

--- 7_objects/main.js
+++ 7_objects/main.js
@@ -5,3 +5,15 @@ var people = {
 }
 
 console.log(people)
+console.log(people.age)
+console.log(people['name'])
+
+function user(id, name, family, age){
+    this.id = id
+    this.name = name
+    this.family = family
+    this.age = age
+}
+
+var p1 = new user(1, "jack", "williams", 25)
+console.log(p1)
