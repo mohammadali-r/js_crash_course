efb14d086541158bd410bfb2fa5bee7aa7ba43dd
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-12-27 21:51:38 +0330

    feat: add 8_arrays directory (#8)
    Co-authored-by: mohammadalirohanian <rohanian021@gmail.com>

---
+++ 8_arrays/main.js
@@ -0,0 +1,11 @@
+// how to declare arrays
+var fruits = new Array("banana", "pineapple", "grape", "orange")
+var cars = ["bmw", "audi", "mercedes benz", "volvo"]
+console.log(fruits)
+console.log(cars)
+console.log(fruits[2])
+console.log(fruits.length)
+
+var new_fruits = new Array("apple", "kiwi", "coconut")
+var a = fruits.concat(new_fruits) // concatenate two array 
+console.log(a)
