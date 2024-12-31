142896ac99b86f4973cb9dac6cd3e96a652de798
MohammadAli <rohanian0803@gmail.com>
2024-12-31 18:56:57 +0330

    feat: add dom stuff to 9_dom

--- 9_dom/main.js
+++ 9_dom/main.js
@@ -1,4 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var hello = "Hello World !";
+//   document.body.innerHTML = hello;
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
-  var hello = "Hello World !";
-  document.body.innerHTML = hello;
+  var elem = document.getElementById("p1");
+  var first_name = prompt("Enter your name:", "My friend");
+  elem.innerHTML = first_name;
 });
